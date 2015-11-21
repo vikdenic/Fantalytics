92d5cae138bfea04313d6b5f1f16a337641ace3d
vik denic <vik.denic@gmail.com>
2015-11-21 17:56:16 -0600

    successfully retreive data from probball api

--- Fantalytics/ViewController.swift
+++ Fantalytics/ViewController.swift
@@ -7,19 +7,30 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
+    let teamURL = "http://api.probasketballapi.com/team"
+    let myAPIkey = "h8Eb1BCDqRgVU3ZcLvTIl5NzM9FnSQif"
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let paramaters = ["api_key" : myAPIkey, "team_abbrv" : "BOS"]
+
+        Alamofire.request(.POST, teamURL, parameters: paramaters)
+            .responseJSON { response in
+
+                print(response.request)  // original URL request
+                print(response.response) // URL response
+                print(response.data)     // server data
+                print(response.result)   // result of response serialization
+
+                if let JSON = response.result.value {
+                    print("JSON: \(JSON)")
+                }
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
