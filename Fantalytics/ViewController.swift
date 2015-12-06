3ad2ceae674206ad638b60075a1f52ed50f0f53c
vik denic <vik.denic@gmail.com>
2015-12-06 01:09:55 -0600

    connect to parse

--- Fantalytics/ViewController.swift
+++ Fantalytics/ViewController.swift
@@ -13,21 +13,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        ProBballManager.getGamesForDate("2015-12-04", season: "2015") { (games) -> Void in
-            guard let someGames = games else {
-                //handle no games
-                print("Games came back nil")
-                return
-            }
-            print(someGames)
-        }
-
-        let testObject = PFObject(className: "TestObject")
-        testObject["foo"] = "bar"
-        testObject.saveInBackgroundWithBlock { (success: Bool, error: NSError?) -> Void in
-            print("Object has been saved.")
-        }
     }
 }
 
